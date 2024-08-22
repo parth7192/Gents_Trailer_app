3110313f9ff3939198906d72a996fb6a7a3a3754
Dk-variya007 <variyadivyesh79@gmail.com>
2024-08-22 13:10:28 +0530

    project add

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,34 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gents_trailer/firebase_options.dart';
-import 'package:gents_trailer/my_app/my_app.dart';
+import 'package:gents_trailer/view/auth/authentication.dart';
+import 'package:gents_trailer/view/home_screen.dart';
 import 'package:get/get.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gents Tailor',
+      home: const MyHomePage(), // Set the initial screen
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => const MyHomePage(),
+        ),
+        GetPage(name: '/', page: () => const AuthScreen())
+      ],
+    );
+  }
 }
---
+++ lib/view/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Get.toNamed('/second');
+          },
+          child: const Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
